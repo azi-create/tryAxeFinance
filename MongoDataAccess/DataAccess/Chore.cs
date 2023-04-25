227f6571b5840264d84cf0d418a6c7e62d30f5be
Aziz Jellouli <jellouliaziz1@gmail.com>
2023-04-25 10:18:58 +0200

    comparison

--- MongoDataAccess/DataAccess/Chore.cs
+++ MongoDataAccess/DataAccess/Chore.cs
@@ -18,25 +18,35 @@ using SharpCompress.Compressors.Xz;
 using System.ComponentModel;
 using System.IO;
 using System.Reflection.Metadata;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.InteropServices;
 
 namespace MongoDataAccess.DataAccess;
 
 
 public class Chore
 {
-    public  JToken AddChangedAttribute(JToken original, JToken modified)
+    public  (JToken, JToken) AddChangedAttribute(JToken original, JToken modified)
     {
+        List<string> listAttributs = new List<string>();
+        listAttributs.Add("CreatedDate");
+        listAttributs.Add("UpdatedDate");
+        listAttributs.Add("Id");
+        List<string> listID = new List<string>();
+        listID.Add("Vocabularies");
         var originalObject = original.ToObject<JObject>();
         var modifiedObject = modified.ToObject<JObject>();
-        AddChangedAttributeRecursive(originalObject, modifiedObject);
+        AddChangedAttributeRecursive(originalObject, modifiedObject, listAttributs,listID);
         string jsonStringOne = JsonConvert.SerializeObject(originalObject);
-        
+        JToken originalJToken = JToken.Parse(jsonStringOne);
+        string jsonStringTwo = JsonConvert.SerializeObject(modifiedObject);
+        JToken modifiedJToken = JToken.Parse(jsonStringTwo);
 
-        JToken jTokenOne = JToken.Parse(jsonStringOne);
-        return jTokenOne;
+        return (originalJToken, modifiedJToken);
     }
 
-    private  void AddChangedAttributeRecursive(JObject originalObject, JObject modifiedObject)
+    private  void AddChangedAttributeRecursive(JObject originalObject, JObject modifiedObject, List<string> listAttributs, List<string> listID)
     {
         foreach (var property in modifiedObject.Properties())
         {
@@ -44,28 +54,100 @@ public class Chore
             var originalValue = originalObject[property.Name];
             var modifiedValue = modifiedObject[property.Name];
 
-            if (originalValue != null && modifiedValue != null)
+
+
+            if (originalValue != null && modifiedValue != null && !listAttributs.Contains(property.Name))
             {
-                if (modifiedValue.Type == JTokenType.Object)
+                if (originalValue.Type == JTokenType.Object)
                 {
-         
-                    AddChangedAttributeRecursive((JObject)originalValue, (JObject)modifiedValue);
+                    AddChangedAttributeRecursive((JObject)originalValue, (JObject)modifiedValue, listAttributs, listID);
                 }
+
+            
+
                 else if (modifiedValue.Type == JTokenType.Array)
                 {
                     var originalArray = (JArray)originalValue;
                     var modifiedArray = (JArray)modifiedValue;
 
-                    if (originalArray.Count == modifiedArray.Count)
+   
+
+                        // eli mawjoudin fi wahda w mch mawjoudin f lokhra
+                        var result1 = modifiedArray.Where(x => !originalArray.Any(y => JToken.DeepEquals(x, y)));
+                        JArray array = JArray.FromObject(result1);
+                        var result2 = originalArray.Where(x => !modifiedArray.Any(y => JToken.DeepEquals(x, y)));
+                        JArray array2 = JArray.FromObject(result2);
+
+                 
+                    foreach (JObject obj1 in originalArray)
                     {
-                        for (int i = 0; i < originalArray.Count; i++)
-                        {
-                            if (modifiedArray[i].Type == JTokenType.Object)
+
+                        // ken aandou id compari
+                        if (listID.Contains(property.Name) ) {
+                            Console.WriteLine("works on " + property.Name);
+
+                            string id = obj1["Id"].ToString();
+                            JObject obj2 = modifiedArray.FirstOrDefault(o => o["Id"].ToString() == id) as JObject;
+
+                            if (obj2 != null)
                             {
-                                AddChangedAttributeRecursive((JObject)originalArray[i], (JObject)modifiedArray[i]);
+                                if (JToken.DeepEquals(obj1, obj2)){obj1["changed"] = "same with same id";} 
+                                else { obj1["changed"] = "changed with same id"; }
                             }
+
+
                         }
+                       
+                        //ken fama added zid
+                        else
+                            {
+                                if (array != null)
+                                {
+                                    for (int i = 0; i < modifiedArray.Count; i++)
+                                    {
+                                        if (array.Any(j => j.ToString() == modifiedArray[i].ToString()))
+                                        {
+                                            modifiedArray[i]["changed"] = "added";
+                                        }
+                                    }
+                                }
+                                //ken fama deleted zid
+
+                                if (array2 != null)
+                                {
+                                    for (int i = 0; i < originalArray.Count; i++)
+                                    {
+
+                                        if (array2.Any(j => j.ToString() == originalArray[i].ToString()))
+                                        {
+
+                                            originalArray[i]["changed"] = "deleted";
+
+                                        }
+                                    }
+                                }
+                            }
+                        
                     }
+
+             
+                    for (int i = 0; i < originalArray.Count; i++)
+                    {
+                        if (originalArray[i].Type == JTokenType.Object)
+                        {
+                            if (i<modifiedArray.Count)
+                            {
+                                AddChangedAttributeRecursive((JObject)originalArray[i], (JObject)modifiedArray[i], listAttributs, listID);
+
+                            }
+
+                        }
+                    }   
+                     
+                 
+
+
+
                 }
                 else if (!JToken.DeepEquals(originalValue, modifiedValue))
                 {
@@ -73,10 +155,10 @@ public class Chore
 
 
 
-                    if (originalValue.Parent.Parent["changed"].HasValues == false)
+                    if (originalValue.Parent.Parent.SelectToken("changed") == null  ||  originalValue.Parent.Parent["changed"].HasValues.ToString() == "same")
                      
                     {
-                        originalValue.Parent.Parent["changed"] = true;
+                        originalValue.Parent.Parent["changed"] = "changed";
                     }
 
 
@@ -85,53 +167,16 @@ public class Chore
                 {
                     if (originalValue.Parent.Parent.SelectToken("changed") == null)
                     {
-                        originalValue.Parent.Parent["changed"] = false;
+                        originalValue.Parent.Parent["changed"] = "same";
                     }
+
+
+
+
                 }
             }
-            
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
--- mongodb try/Program.cs
+++ mongodb try/Program.cs
@@ -1,59 +1,21 @@
 ﻿using MongoDataAccess.DataAccess;
-using MongoDB.Driver;
-using mongodb_try;
 using MongoDataAccess.Models;
-using MongoDB.Bson;
-using System.Xml.Linq;
 using MongoDataAccess.Models.Enums;
-using System.Threading.Channels;
-using System.Collections.Generic;
-using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using JsonDiffPatchDotNet;
-using System.Collections;
-using System.Reflection.Metadata;
-using Json.Comparer;
-using System.Xml;
-using Amazon.Runtime.Internal.Transform;
-using System.Reflection;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Runtime.InteropServices.JavaScript;
 using MongoDataAccess.Models.VocabularyEntites;
-
 string connectionString = "mongodb://localhost:27017";
 string databaseName = "friendsList";
-//string collectionName = "people";
-
-var client = new MongoClient(connectionString);
-var db = client.GetDatabase(databaseName);
-//var collection = db.GetCollection<PersonMode>(collectionName);
 
 
 
 
 
-ChoreDataAccess mth = new ChoreDataAccess();
+
+
 Chore mdh = new Chore();
 
 
-//var changes = await mth.GetAllChanges();
-
-//foreach (var result in changes.ToList())
-//{
-//    Console.WriteLine($"{result.Id}: {result.timeOfOperation} {result.operation}");
-//}
-
-//var change = new changesModel{ timeOfOperation = "12:02:2023", operation = "get"};
-
-//await mth.CreateChange(change);
-
-
-//var person = new userModel { Id= "64021379004b4a832b5e2f46",  lastName = "mch iron", name = "mch man" };
-
-//await mth.CreateUser(new userModel() { name = "Rachel", lastName = "green" });
-
-//await mth.UpdateUser(person);
 var context = new ContextType();
 
 var resultsOne = new Result();
@@ -83,83 +45,73 @@ var VocabularyTwo = new Vocabulary();
 VocabularyTwo.Name = "vocab2";
 
 List<Vocabulary> outputs = new List<Vocabulary>();
-outputs.Add(VocabularyOne);
-outputs.Add(VocabularyTwo);
+
+
+
+var Vocabulary0 = new Vocabulary();
+Vocabulary0.Name = "number 0";
+
+
+var Vocabulary7 = new Vocabulary();
+Vocabulary7.Name = "number 7";
 
 
 
 
 
 var Vocabulary3 = new Vocabulary();
-Vocabulary3.Name = "jdid";
+Vocabulary3.Name = "number 3";
 var Vocabulary4 = new Vocabulary();
-VocabularyTwo.Name = "new";
+Vocabulary4.Name = "number 4";
+var Vocabulary5 = new Vocabulary();
+Vocabulary5.Name = "number 5";
+var Vocabulary6 = new Vocabulary();
+Vocabulary6.Name = "number 6";
 List<Vocabulary> newout = new List<Vocabulary>();
-newout.Add(Vocabulary3);
+newout.Add(Vocabulary7);
 newout.Add(Vocabulary4);
+newout.Add(Vocabulary5);
+newout.Add(Vocabulary6);
+
+
+outputs.Add(Vocabulary7);
+outputs.Add(Vocabulary3);
+outputs.Add(Vocabulary4);
+outputs.Add(Vocabulary0);
 
 
 
-
-//el node
 var node = new Node();
 
 
-var rulsetOne = new Ruleset("name rulset 3", "description rulset 2",  rules, 1, outputs, node, listResult, "output type 1","same");
+var rulsetOne = new Ruleset("name rulset ", "description rulset 2",  rules, 1, outputs, node, listResult, "output type 1");
+var rulsetTwo = new Ruleset("name rulset 2", "description rulset 2 ", rules, 2, outputs, node, listResult, "output type 2");
+var rulsetThree = new Ruleset("name rulset 3", "description rulset 3 ", rules, 3, outputs, node, listResult, "output type 3");
+var rulsetFour = new Ruleset("name rulset 4", "description rulset 4", rules, 4, outputs, node, listResult, "output type 4");
 
-var rulsetTwo = new Ruleset("name rulset 9", "description rulset 9 ", rules, 2, outputs, node, listResult, "output type 1", "same");
+
 List<Ruleset> rulesets = new List<Ruleset>();
 rulesets.Add(rulsetOne);
 rulesets.Add(rulsetTwo);
+rulesets.Add(rulsetFour);
 
+List<Ruleset> newRulesets = new List<Ruleset>();
+newRulesets.Add(rulsetOne);
+newRulesets.Add(rulsetThree);
 
 
 
 
 var content = new Content(outputs, rulesets);
-var newContent = new Content(newout, rulesets);
+var newContent = new Content(newout, newRulesets);
 
-var policyOne = new Policy( "same" ,"name policy 3", "description policy 3", "owner policy ", "statu policy", "version policy 3",content, listResult);
-var policyTwo = new Policy("same", "name policy 4", "description policy 4", "owner policy ", "statu policy", "version policy 2 ", newContent, listResult);
-
-
-
-//////bool isEqual = policyOne.Equals(policyTwo);
-//////Console.WriteLine(isEqual);
-
-var policies = new List<Policy>();
-policies.Add(policyOne);
-policies.Add(policyTwo);    
-
-var policySetTwo = new PolicySet("name 2", "description 2", context,  "owner 2" , policies);
-
-//var policySetTOne = new PolicySet("name 1", "description 1", context, "owner 1", policies);
+var policyOne = new Policy("name policy 3", "description policy 3", "owner policy ", "statu policy", "version policy 3",content, listResult);
+var policyTwo = new Policy("name policy 4", "description policy 4", "owner policy ", "statu policy", "version policy 2 ", newContent, listResult);
 
 
-//Console.WriteLine(policyOne.Name);
-//await mth.CreatePolicySet(policySetOne);
-//await mth.CreatePolicySet(policySetTwo);
-
-
-
-
-
-
-//var NewPolicies = await mth.GetAllPolicySet();
-var resultOne = mth.GetPolicySetByName("name 1");
-var resultTwo = mth.GetPolicySetByName("name 2");
-
-
-//var resultTwo = mth.GetPolicySetByName("name 3");
 
 string jsonStringOne = JsonConvert.SerializeObject(policyOne);
 string jsonStringTwo = JsonConvert.SerializeObject(policyTwo);
-
-
-
-
-
-
 
 
 JToken jTokenOne = JToken.Parse(jsonStringOne);
@@ -168,236 +120,11 @@ JToken jTokenTwo = JToken.Parse(jsonStringTwo);
 
 
 
-
-
-/////////////////////////////
-
-string originalJson = @"
-
-        {
-
-            'name': 'John',
-
-            'age': 30,
-
-          
-
-            'pets': [
-
-                {
-
-                    'name': 'Fido',
-
-                    'type': 'dog',
-                
-
-                },
-
-                {
-
-                    'name': 'Mittens',
-
-                    'type': 'cat',
-                    
-
-                },
-                {
-
-                    'name': 'mohssen',
-
-                    'type': 'tiger',
-                   
-                }
-
-            ]
-
-        }";
-
-
-
-// JSON modifié
-
-string modifiedJson = @"
-
-        {
-
-            'name': 'Jane',
-
-            'age': 30,
-
-         
-
-            'pets': [
-
-                {
-
-                    'name': 'Fido',
-
-                    'type': 'dog',
-
-                   
-
-                },
-
-                {
-
-                    'name': 'Mittens',
-
-                    'type': 'cat',
-
-                    
-
-                },
-
-                {
-
-                    'name': 'Spot',
-
-                    'type': 'dog',
-
-                    
-
-                }
-
-            ]
-
-        }";
-
-
-
-
-
-
-
-
-
-
-
-
-// Convertir les deux JSON en objets JToken
-
-JToken originalToken = JToken.Parse(originalJson);
-
-JToken modifiedToken = JToken.Parse(modifiedJson);
-
-
-
-JToken result = mdh.AddChangedAttribute(jTokenOne, jTokenTwo);
-Console.WriteLine("result = " + result);
-
-
-
-
-
-
-// Trouver les différences entre les deux objets JToken
-
-//JToken diff = mth.FindDiff(originalToken, modifiedToken);
-//Console.WriteLine(diff);
-
-// Ajouter l'attribut "changed" à chaque nœud qui a été modifié
-
-//mth.MarkChanges(modifiedToken, diff);
-
-
-
-//////// Convertir l'objet JToken modifié en JSON
-
-//////string modifiedJsonWithChanges = modifiedToken.ToString();
-
-
-
-//////// Afficher le JSON modifié avec les changements
-
-//////Console.WriteLine(modifiedJsonWithChanges);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Dictionary<string, object> dictionaryOne = jTokenOne.ToObject<Dictionary<string, object>>();
-Dictionary<string, object> dictionaryTwo = jTokenTwo.ToObject<Dictionary<string, object>>();
-
-//mth.Compare(dictionaryOne, dictionaryTwo);
-
-Dictionary<string, object> dictionaryResult = mth.ParcourDics(dictionaryOne, dictionaryTwo);
-
-//mth.Comparer(dictionaryOne, dictionaryTwo);
-
-
-
-
-
-
-var jdp = new JsonDiffPatch();
-var patch = jdp.Diff(jTokenOne, jTokenTwo);
-
-
-
-
-
-string json = JsonConvert.SerializeObject(dictionaryResult);
-JToken jToken = JToken.Parse(json);
-//Console.WriteLine(jToken);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(JToken Result1, JToken Result2) = mdh.AddChangedAttribute(jTokenOne, jTokenTwo);
+
+Console.WriteLine("result original = " + Result1);
+//Console.WriteLine("//////////////");
+//Console.WriteLine("result modif  = " + Result2);
 
 
 
